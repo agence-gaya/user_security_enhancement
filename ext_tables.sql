ef762c065b18fe9c75030923c6512ae7988cc0ac
Benoit Chenu <bchenu@gaya.fr>
2022-11-29 15:27:17 +0100

    [BUGFIX] Error during fe_user creation with extbase and MySQL in STRICT_TRANS_TABLES mode

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
-	old_password_list text DEFAULT '' NOT NULL,
+	old_password_list text,
 	login_blocked_endtime int(11) unsigned DEFAULT '0' NOT NULL,
 	login_attempt_failure int(11) DEFAULT '0' NOT NULL,
 );
